45802e10b156691e773324b521a527620c3004bf
protoxpire0 <fahim.shahreer95@gmailcom>
2016-10-21 14:04:53 -0400

    added the new stucture

--- scripts/sample.py
+++ scripts/sample.py
@@ -27,7 +27,7 @@ scorefxn.set_weight(vdw, 1.0)
 #scorefxn.set_weight(cbeta, 1.0)
 
 #set up simulation parameters
-ncycles = 50000
+ncycles = 100000
 kT = 1.0
 mc = MonteCarlo(p, scorefxn, kT)
 
@@ -36,8 +36,8 @@ movemap = MoveMap()
 movemap.set_bb(True)
 
 # fragment mover
-fragset = ConstantLengthFragSet(9)
-fragset.read_fragment_file("../fraglibs/1LGL_9.txt")
+fragset = ConstantLengthFragSet(3)
+fragset.read_fragment_file("../fraglibs/1LGL_3.txt")
 cost = GunnCost()
 
 frag_mover = SmoothFragmentMover(fragset, movemap, cost)
